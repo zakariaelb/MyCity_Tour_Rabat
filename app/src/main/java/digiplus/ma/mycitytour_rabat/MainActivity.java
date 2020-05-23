5e5fcfa122e9bf3edf4a3ed1a2883e5a77e5c3ee
zelbatmi <zakari_a@icloud.com>
2020-05-23 17:59:34 +0000

    1.0

--- app/src/main/java/digiplus/ma/mycitytour_rabat/MainActivity.java
+++ app/src/main/java/digiplus/ma/mycitytour_rabat/MainActivity.java
@@ -1,6 +1,7 @@
 package digiplus.ma.mycitytour_rabat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
@@ -9,6 +10,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /**
+        /* Set ativity content to use the activity_main.xml
+        */
+
         setContentView(R.layout.activity_main);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.Root_View_Pager_ID);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+
+        // Find the tab layout that shows the tabs
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.TableLayout_ID);
+
+        // Connect the tab layout with the view pager. This will
+        //   1. Update the tab layout when the view pager is swiped
+        //   2. Update the view pager when a tab is selected
+        //   3. Set the tab layout's tab names with the view pager's adapter's titles
+        //      by calling onPageTitle()
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/digiplus/ma/mycitytour_rabat/MainView_Adapter.java
@@ -0,0 +1,52 @@
+package digiplus.ma.mycitytour_rabat;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+/**
+ * Main Adapter
+ */
+public class MainView_Adapter extends FragmentPagerAdapter {
+
+    private Context mContext;
+
+    public MainView_Adapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new NumbersFragment();
+        } else if (position == 1) {
+            return new FamilyFragment();
+        } else if (position == 2) {
+            return new ColorsFragment();
+        } else {
+            return new PhrasesFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
+    }
+}
